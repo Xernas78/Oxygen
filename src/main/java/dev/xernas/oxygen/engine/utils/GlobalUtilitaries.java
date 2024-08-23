76f89254d0b993797777143f32f501176b899077
Xernas <Xernas78@gmail.com>
2024-08-24 01:39:19 +0200

    Added requireObject

--- src/main/java/dev/xernas/oxygen/engine/utils/GlobalUtilitaries.java
+++ src/main/java/dev/xernas/oxygen/engine/utils/GlobalUtilitaries.java
@@ -1,6 +1,7 @@
 package dev.xernas.oxygen.engine.utils;
 
 import dev.xernas.oxygen.engine.Behavior;
+import dev.xernas.oxygen.engine.SceneObject;
 import dev.xernas.oxygen.exception.OxygenException;
 
 import java.util.function.Consumer;
@@ -15,6 +16,17 @@ public class GlobalUtilitaries {
             return (T) behavior;
         } catch (ClassCastException e) {
             throw new OxygenException("Behavior is not the right type");
+        }
+    }
+
+    public static <T> T requireObject(SceneObject object, String error) throws OxygenException {
+        if (object == null) {
+            throw new OxygenException(error);
+        }
+        try {
+            return (T) object;
+        } catch (ClassCastException e) {
+            throw new OxygenException("Object is not the right type");
         }
     }
 
